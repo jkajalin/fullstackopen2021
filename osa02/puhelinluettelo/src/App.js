ffa2af727305eafde4672e15fc70ecd129739aeb
Jussi Kajalin <jkajalin@gmail.com>
2021-10-20 11:04:13 +0300

    t2.7 step2 puhelinluettelo

--- osa02/puhelinluettelo/src/App.js
+++ osa02/puhelinluettelo/src/App.js
@@ -1,27 +1,49 @@
 import React, { useState } from 'react'
 
+const Person = ({person}) =>{
+  return(
+    <li>{person.name}</li>
+  )
+}
+
 const App = () => {
   const [ persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
 
-  const Person = ({person}) =>{
-    return(
-      <li>{person.name}</li>
-    )
-  }
+  
 
   const addPerson =(event) =>{
     event.preventDefault()
     const personObject ={
       name: newName
       
+    }    
+    const nameExist= persons.find(person => person.name === personObject.name)
+    if( nameExist ){
+      console.log("Exist!")
+      window.alert(`${newName} is already added to phonebook`)
+    }else{
+      setPersons(persons.concat(personObject))
+      setNewName('') 
+         
     }
-    setPersons(persons.concat(personObject))
-    setNewName('')
+      
+    
   }
   const handleNameChange = (event) => {
+    //const nameExist= persons.find(person => person.name === event.target.value)
+    //console.log(nameExist.name)
+    //console.log(nameExist)
+    /*
+    if(nameExist){
+      console.log("Exist!")
+      window.alert(`${newName} is already added to phonebook`)
+    }else{
+      
+      
+    }*/
     setNewName(event.target.value)
   }
 
