51f6afe1c8f6a2938d21bb3a4d63e71a83a8e2cd
Jussi Kajalin <jkajalin@gmail.com>
2021-10-28 19:37:24 +0300

    t2.11 step6 puhelinluettelo

---
+++ osa02/maidentiedot/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+const App = () => (
+  <div>
+    <p>Hello world lo23</p>
+  </div>
+)
+
+export default App
--- osa02/puhelinluettelo/src/App.js
+++ osa02/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = ({person}) =>{
   return(
@@ -41,17 +42,24 @@ const Persons = ({personsFiltered}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456' },
-    { name: 'Ada Lovelace', phone: '39-44-5323523' },
-    { name: 'Dan Abramov', phone: '12-43-234345' },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newPhone, setNewPhone ] = useState('')
   const [ filterText, setFilterText ] = useState('')
   
   const [ personsFiltered, setPersonsFiltered] = useState(persons)  
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+        setPersonsFiltered(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
   const addPerson =(event) =>{
     event.preventDefault()
