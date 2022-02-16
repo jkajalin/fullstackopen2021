b8b51a4ff9039aa81a84a5e32be1a0d239859e94
Jussi Kajalin <jkajalin@gmail.com>
2022-02-16 17:12:42 +0200

    4.2 blogilista, step2

---
+++ osa04/blogilista/app.js
@@ -0,0 +1,17 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blog-router')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
