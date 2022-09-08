bcc51e7be88603966ec69af98dafddb75cfc4ef9
Jussi Kajalin <jkajalin@gmail.com>
2022-09-08 19:45:31 +0300

    osa08 -t 8.13.-8.15

--- osa08/library-backend/library-backend.js
+++ osa08/library-backend/library-backend.js
@@ -1,5 +1,22 @@
+require('dotenv').config()
 const { ApolloServer, gql, UserInputError } = require('apollo-server')
 const { v1: uuid } = require('uuid')
+const mongoose = require('mongoose')
+const Book = require('./models/book')
+const Author = require('./models/author')
+const author = require('./models/author')
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
 
 let authors = [
   {
@@ -91,6 +108,16 @@ let books = [
 
 const typeDefs = gql`
 
+type User {
+  username: String!
+  favoriteGenre: String!
+  id: ID!
+}
+
+type Token {
+  value: String!
+}
+
   type Author {
     name: String!
     born: Int
@@ -101,7 +128,7 @@ const typeDefs = gql`
   type Book {
     title: String!
     published: Int!
-    author: String!
+    author: Author!
     genres: [String]!
     id: ID!
   }
@@ -134,29 +161,71 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
-    allBooks: ( root, args ) => {
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: () => Author.collection.countDocuments(),
+    allBooks: async ( root, args ) => {
+      authors = await Author.find({})
+      books= await Book.find({})
+      
+      books.map( b => { b.author = authors.find( a => a.id.toString() === b.author.toString() )}  )
       if(!args.author && !args.genre){
+        
+        //return Book.find({}) // setting author field not implemented yet
         return books
       }
       if(args.genre && !args.author){
-        return books.filter( p => p.genres.find( g => g === args.genre ) )
+        return books.filter( b => b.genres.find( g => g === args.genre ) )
+        //return Book.find({ genres: { $in: args.genre}}) //.filter( p => p.genres.find( g => g === args.genre ) )
+        // setting author field not implemented yet...
       }
       if(args.genre && args.author){
-        return books.filter( p => p.author === args.author ).filter( p => p.genres.find( g => g === args.genre ) )
+        //console.log('books by genre from author')
+        const authr1 = await Author.findOne({ name: args.author })
+        console.log(authr1.name+' found, seek for books in genre')
+        const boks = await Book.find({author: authr1})
+        console.log(boks.length+' books found') // jatka seuraavasta rivistä
+        //console.log(boks+' books')
+        return boks.filter( p => p.genres.find( g => g === args.genre ) ) // fook... why this don't work // it kinda does, author field just still not implemented properly
+        //return Book.find({}) //.filter( p => p.author === args.author ).filter( p => p.genres.find( g => g === args.genre ) )
       }
-      return books.filter( p => p.author === args.author )
+      const authr = await Author.findOne({ name: args.author})  //(await Author.find({})).filter(p => p.name === args.author)
+      console.log(authr.name+' found')
+      //const boks =  await Book.find({})
+      // returnin books where author name is args.author
+      return books.filter( b => b.author.id.toString() === authr.id.toString())
+      //return Book.find({ author: authr}) //boks.filter( p => p.author === authr[0] ) // author: { $in: args.author}
     },
-    allAuthors: () => authors
+    allAuthors: async () => Author.find({})
   },
   Author: { 
-    bookCount: (root) => books.filter( p => p.author === root.name ).length,     
+    bookCount: async (root) => {
+      const authr = await Author.findOne({ name: root.name})
+      return (await Book.find({ author: authr})).length
+    } //books.filter( p => p.author === root.name ).length,     
   },
   Mutation: {
-    addBook: ( root, args ) => {
-      const book = { ... args, id: uuid() }
-            
+    addBook: async( root, args ) => {
+      //const book = { ... args, id: uuid() }
+      const book = new Book( {...args } )
+
+      console.log('author name: '+args.author)
+      let bookAuthor = await Author.findOne({ name: args.author })
+      
+      if(!bookAuthor){
+        const newAuthor = new Author( {name: args.author} )        
+        try{
+          bookAuthor = await newAuthor.save()
+        } catch(error){
+          throw new UserInputError(error.message, { invalidArgs: args, })
+        }
+      }
+      book.author = bookAuthor.id
+      //return book.save()
+      
+      //const book = new Book( {...args, author: new Author({ name: args.author}) })
+
+      //const author = new Author({ name: args.author})
+      /*
       if( books.find( p => p.title === args.title && p.author === args.author ) ){
         throw new UserInputError(`Book titled ${args.title} by author ${args.author} already exist`, { 
           invalidArgs: args.title
@@ -168,16 +237,33 @@ const resolvers = {
         const author = { name: args.author }
         authors = authors.concat(author)
       }
+      */
+      //return book
+      try {
+        await book.save()
+      } catch (error) {
+        throw new UserInputError( error.message, { invalidArgs: args, } )
+      }
+      
       return book
     },
-    editAuthor: (root, args ) => {
-      const author = authors.find( p => p.name === args.name )
+    editAuthor: async (root, args ) => {
+      //const author = authors.find( p => p.name === args.name )
+      const author = await Author.findOne({ name: args.name})
       if (!author){
         return null
       }
-      const updauthor = { ...author, born: args.setBornTo }
-      authors = authors.map( p => p.name === args.name ? updauthor : p)
-      return updauthor 
+      
+      author.born=args.setBornTo
+      //authors = authors.map( p => p.name === args.name ? updauthor : p)
+      //return updauthor
+      
+      try {
+        await author.save()
+      } catch (error) {
+        throw new UserInputError( error.message, { invalidArgs: args, } )
+      }
+      return author
     }
   }
 }
